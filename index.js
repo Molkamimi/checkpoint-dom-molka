e1086255ce03a78045310899966067ca17ee3212
Molkamimi <70590878+Molkamimi@users.noreply.github.com>
2020-09-21 00:02:44 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector('h1').style.color="red";
+document.queryCommandValue('h2').style.backgroundColor="grey";
+document.querySelector("btn btn-link").style.color="red";
+function myFunctionheart1(){
+    document.getElementById("heart1").style.color="red";
+}
